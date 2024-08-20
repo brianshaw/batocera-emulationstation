b7a5b3fdf25db9b633f3ff72eeb8e28cc781c163
Brian Shaw <brian@beclicking.com>
2024-08-20 01:15:54 -0500

    test

--- es-app/src/FileData.cpp
+++ es-app/src/FileData.cpp
@@ -685,7 +685,7 @@ bool FileData::launchGame(Window* window, LaunchGameOptions options)
 	const std::string rom = Utils::FileSystem::getEscapedPath(getPath());
 	const std::string basename = Utils::FileSystem::getStem(getPath());
 
-	Scripting::fireEvent("game-start", rom, basename, getName());
+	Scripting::fireEvent("pre-game-start", rom, basename, getName());
 
 	// run reload es_settings.cfg on game launch
 	Settings::getInstance()->loadFile();
@@ -704,6 +704,9 @@ bool FileData::launchGame(Window* window, LaunchGameOptions options)
 	// InputManager::getInstance()->computeLastKnownPlayersDeviceIndexes();
 	// todo put this in an if settings is set to do this
 	// todo maybe not reload the whole file and target inputs?
+	
+	Scripting::fireEvent("game-start", rom, basename, getName());
+
 
 	time_t tstart = time(NULL);
 
