504bc9689bd71fa7a3cb9ec73022a7201b103b6c
Brian Shaw <brian@beclicking.com>
2024-08-21 10:26:57 -0500

    fix

--- es-app/src/guis/GuiControllersSettings.cpp
+++ es-app/src/guis/GuiControllersSettings.cpp
@@ -188,7 +188,8 @@ GuiControllersSettings::GuiControllersSettings(Window* wnd, int autoSel) : GuiSe
 	addGroup(controllers_group_label);
 	addEntry(_("CONTROLLER ASSIGNMENT PER EMULATOR"), false, [window, this, autoSel]
 	{
-		window->pushGui(new GuiControllersAssignmentsPerEmulator(window, autoSel));
+		GuiControllersAssignmentsPerEmulator::openControllersSettings(window);
+		// window->pushGui(new GuiControllersAssignmentsPerEmulator(window, autoSel));
 		// window->pushGui(new GuiMsgBox(window,
 		// 	_("YOU ARE GOING TO MAP A CONTROLLER. MAP BASED ON THE BUTTON'S POSITION, "
 		// 		"NOT ITS PHYSICAL LABEL. IF YOU DO NOT HAVE A SPECIAL BUTTON FOR HOTKEY, "
